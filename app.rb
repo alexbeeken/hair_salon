19c19e9f158df12163c35e67cca82b3d4e68295d
Alex Beeken <aeb242@gmail.com>
2015-01-23 19:59:20 +0000

    Displays a home page

--- app.rb
+++ app.rb
@@ -1,2 +1,11 @@
 require('sinatra')
 require('sinatra/reloader')
+require('customer')
+require('stylist')
+require('pg')
+require('pry')
+
+get '/' do
+  @all_stylists = Stylist.all()
+  erb(:salon_home)
+end
