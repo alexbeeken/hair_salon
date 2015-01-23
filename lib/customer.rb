fcce92ba0cc33acc437a379b359bc61c60d868c3
Alex Beeken <aeb242@gmail.com>
2015-01-23 19:30:25 +0000

    Delete customers, Find customers by id, Find customers by name, id, stylist_id

--- lib/customer.rb
+++ lib/customer.rb
@@ -27,4 +27,44 @@ class Customer
   define_method(:==) do |another_stylist|
     (self.name.==another_stylist.name)
   end
+  
+  define_method(:delete) do 
+    DB.exec("DELETE FROM customers WHERE id = #{@id};")
+  end
+  
+  define_singleton_method(:get_customers_for_stylist_id) do |id|
+    query = DB.exec("SELECT * FROM customers WHERE stylist_id = #{id};")
+    all = []
+    query.each() do |customer|
+      name = customer.fetch("name")
+      stylist_id = customer.fetch("stylist_id").to_i
+      id = customer.fetch("id").to_i()
+      all.push(Customer.new({:name => name, :stylist_id => stylist_id, :id => id}))
+     end
+    all
+  end
+  
+  define_singleton_method(:find_customer_by_id) do |id|
+  query = DB.exec("SELECT * FROM customers WHERE id = '#{id}';")
+    if query.ntuples == 0
+      return false
+    else
+      name = query[0]["name"]
+      stylist_id = query[0]["stylist_id"]
+      id = query[0]["id"]
+      return Customer.new({:name => name, :stylist_id => stylist_id, :id => id})
+    end
+  end
+  
+define_singleton_method(:find_customer_by_name) do |name|
+    query = DB.exec("SELECT * FROM customers WHERE name = '#{name}';")
+    if query.ntuples == 0
+      return false
+    else
+      name = query[0]["name"]
+      stylist_id = query[0]["stylist_id"]
+      id = query[0]["id"]
+      return Customer.new({:name => name, :stylist_id => stylist_id, :id => id})
+    end
+  end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -24,37 +24,47 @@ end
 
 describe('#==') do
   it 'compares two customers correctly' do
-    test_customer = Customer.new({:name => "Shirley", :stylist_id => 1, :id => nil})
-    test_customer2 = Customer.new({:name => "Shirley", :stylist_id => 1, :id => nil})
-    expect(test_customer).to(eq(test_customer2))
+      test_customer = Customer.new({:name => "Shirley", :stylist_id => 1, :id => nil})
+      test_customer2 = Customer.new({:name => "Shirley", :stylist_id => 1, :id => nil})
+      expect(test_customer).to(eq(test_customer2))
   end
 end
 
-=begin
 describe('#delete') do
     it 'removes customer from the database' do
-    (test_customer = Customer.new({:name => "Shirley", :stylist_id => 0, :id => nil})
-      (test_customer.save()
-        (test_customer2 = Customer.new({:name => "Shirley2", :stylist_id => 0, :id => nil})
-       (test_customer2.save()
-       (test_customer.delete()
-       expect Customer.all).to(eq((test_customer2]))
+      test_customer = Customer.new({:name => "Shirley", :stylist_id => 0, :id => nil})
+      test_customer.save()
+      test_customer2 = Customer.new({:name => "Shirley2", :stylist_id => 0, :id => nil})
+      test_customer2.save()
+      test_customer.delete()
+      expect(Customer.all).to(eq([test_customer2]))
   end
 end
 
-        describe('#find_customer_by_id') do
-        it 'finds a customer by their id' do
-        (test_customer = Customer.new({:name => "Shirley", :stylist_id => 0, :id => nil})
-       (test_customer.save()
-         expect Customer.find_customer_by_id(test_customer.id)).to(eq(test_customer))
+  
+describe('#find_customer_by_id') do
+    it 'finds a customer by their id' do
+       test_customer = Customer.new({:name => "Shirley", :stylist_id => 0, :id => nil})
+       test_customer.save()
+       expect(Customer.find_customer_by_id(test_customer.id)).to(eq(test_customer))
   end
 end
   
-      describe('#find_customer_by_name') do
-      it 'finds a customer by their name' do
-      (test_customer = Customer.new({:name => "Shirley", :stylist_id => 0, :id => nil})
-       (test_customer.save()
-         expect Customer.find_customer_by_name(test_customer.name)).to(eq(test_customer))
+describe('#find_customer_by_name') do
+    it 'finds a customer by their name' do
+      test_customer = Customer.new({:name => "Shirley", :stylist_id => 0, :id => nil})
+      test_customer.save()
+      expect(Customer.find_customer_by_name(test_customer.name)).to(eq(test_customer))
   end
 end
-=end
+
+describe('#get_customers_for_stylist_id') do
+  it 'returns all customers associated with that stylist id' do
+    test_customer = Customer.new({:name => 'Shirley', :stylist_id => 1, :id => nil})
+    test_customer.save()
+    expect(Customer.get_customers_for_stylist_id(1)).to(eq([test_customer]))
+  end
+end
+
+
+      
