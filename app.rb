3e8615fa9bbfc3265b5615b834fc780599db878c
Alex Beeken <aeb242@gmail.com>
2015-01-23 20:06:28 +0000

    Display home page, add stylists to page

--- app.rb
+++ app.rb
@@ -1,11 +1,26 @@
 require('sinatra')
 require('sinatra/reloader')
-require('customer')
-require('stylist')
+require('./lib/customer')
+require('./lib/stylist')
 require('pg')
 require('pry')
+
+configure :development do
+set :bind, '0.0.0.0'
+set :port, 3000
+end
+
+DB = PG::Connection.open(:dbname => 'salon')
+
 
 get '/' do
   @all_stylists = Stylist.all()
   erb(:salon_home)
+end
+
+post '/add_stylist' do
+  name = params.fetch('name')
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  redirect'/'
 end
